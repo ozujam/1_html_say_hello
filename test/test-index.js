f9c56738074b945236f0efb4c62922f7d6be3aba
James Min <jmin499@gmail.com>
2020-10-16 16:02:26 -0400

    hello test works

--- test/test-index.js
+++ test/test-index.js
@@ -1,10 +1,25 @@
-const hello = require('../index')
 const { expect } = require('chai');
-// const chai = require('chai')
+const chai = require('chai');
+const { JSDOM } = require('jsdom');
 
+chai.use(require('chai-dom'));
+require('jsdom-global')();
 
-describe('Hello World', () => {
-    it('hello function should return hello world', () => {
-      expect(hello()).to.equal('hello world')
+describe('HTML', () => {
+
+    beforeEach((done) => {
+      JSDOM.fromFile('index.html')
+      .then((dom) => {
+        global.document = dom.window.document
+      })
+      .then(done, done);
+    });
+  
+    describe('Heading', () => {
+      it("DOM should have a h1 element that saids Hello World!'", () => {
+        let element = document.getElementById('hello')
+        expect(element).to.have.text('hello world')
+      })
     })
+   
   })
